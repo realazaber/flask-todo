c74abedb6387f8185db4dd32c234d09e6380b688
AZ <47402666+realazaber@users.noreply.github.com>
2023-09-09 19:01:53 +0200

    Get todos work

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv  
+import os
 
 app = Flask(__name__)
 
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
+# Initialize SQLAlchemy with the Flask app
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(256), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'   
+
 @app.route('/', methods=['GET'])
 def getTodos():
-    return "Get todos"
+    # Query all todos from the database
+    todos = Todo.query.all()
+
+    # Serialize the todos to a list of dictionaries
+    serialized_todos = [{
+        'id': todo.id,
+        'title': todo.title,
+        'description': todo.description,
+        'completed': todo.completed
+    } for todo in todos]
+
+    # Return the serialized todos as JSON response
+    return jsonify(serialized_todos)
 
 @app.route('/create', methods=['POST'])
 def createTodo():
