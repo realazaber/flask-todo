f06b5d944d4b30cc3b2b00782c70c3aed10ad299
AZ <47402666+realazaber@users.noreply.github.com>
2023-09-10 17:27:52 +0200

    Display for todos

---
+++ frontend/src/components/TodosList.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import Todo from "./Todo";
+import { ITodo } from "../interfaces/todo";
+import { TodoContext } from "@/pages/_app";
+
+const TodosList = () => {
+  const { todos } = useContext(TodoContext) || { todos: [] }; // Initialize with an empty array when the context is undefined
+
+  return (
+    <section className="my-3">
+      <div className="container">
+        {todos.length === 0 ? (
+          <p>No todos found</p>
+        ) : (
+          <div className="rounded-md text-white p-5 w-full flex flex-wrap justify-center">
+            {todos.map((todo: ITodo) => (
+              <Todo
+                key={todo.id}
+                id={todo.id}
+                title={todo.title}
+                description={todo.description}
+                completed={todo.completed}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default TodosList;
