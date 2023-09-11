7c1c6cb1dfe72c2ca97ac046fd625698ce9fb8d1
AZ <47402666+realazaber@users.noreply.github.com>
2023-09-11 16:15:30 +0200

    Footer links to azaber

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -9,7 +9,9 @@ const currentDate = new Date().getFullYear();
 const Footer = (props: Props) => {
   return (
     <footer className="bg-pri text-sec text-center py-3 mt-auto">
-      Created by Azaber {currentDate}
+      <a href="https://www.azaber.com" target="_blank">
+        Created by Azaber {currentDate}
+      </a>
     </footer>
   );
 };
